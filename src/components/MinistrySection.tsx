fdde02502499d0cb5a67a6839fc30dc055c7e6d8
Tjitouua <veriimapoha@gmail.com>
2025-12-22 14:56:31 +0200

    Still busy with the Ministries page

--- src/components/MinistrySection.tsx
+++ src/components/MinistrySection.tsx
@@ -3,6 +3,52 @@ import MinistryCard from "../ui/MinistryCard";
 
 
 const MinistrySection = () => {
+
+
+
+    const MinistryList = [
+        {
+           title: "Home Affairs", 
+           services: 3,
+           desc: "Responsible for civil registration, immigration, national documents, and internal security.",
+           ministry: "Ministry of Home Affairs, Immigration, Safety and Security",
+        },
+        
+         {
+            title: "Works & Transport", 
+            services: 1,
+            desc: "Manages transport licensing, vehicle registration, and public infrastructure.",
+            ministry: "Ministry of Works and Transport",
+         },
+         {
+            title: "Land & Agriculture", 
+            services: 2,
+            desc: "Handles land registration, agricultural services, and water resource management.",
+            ministry: "Ministry of Agriculture, Water and Land Reform",
+         },
+         {
+            title: "Foreign Affairs", 
+            services: 1,
+            desc: "Manages passport issuance, visa services, and international relations.",
+            ministry: "Ministry of International Relations and Cooperation",
+         },
+         {
+            title: "Trade & Industry", 
+            services: 4,
+            desc: "Oversees business registration, trade licensing, and industrial development.",
+            ministry: "Ministry of Industrialisation and Trade",
+         },
+         {
+            title: "Social Welfare", 
+            services: 1,
+            desc: "Deals with social welfare grants, poverty relief, and gender equality programs.",
+            ministry: "Ministry of Gender Equality, Poverty Eradication and Social Welfare",
+         },
+    ];
+
+
+
+
      return (
         <div className="w-full min-h-screen flex flex-col px-15 py-6 gap-4">
                 {/* Search part */}
@@ -14,12 +60,15 @@ const MinistrySection = () => {
                 <div className="w-full min-h-screen flex text-gray-500 text-sm flex-col gap-3 py-3">
                 <label>Showing 8 ministries</label>
                 <div className="w-full py-2 flex flex-wrap items-center gap-4 justify-between">
-                    <MinistryCard />
-                    <MinistryCard />
-                    <MinistryCard />
-                    <MinistryCard />
-                    <MinistryCard />
-                    <MinistryCard />
+                    {MinistryList.map((ministry, index) => (
+                    <MinistryCard
+                        key = {index}
+                        title = {ministry.title}
+                        services = {ministry.services}
+                        desc = {ministry.desc}
+                        ministry = {ministry.ministry}
+                    />
+                    ))}
                 </div>
             </div>
 
--- src/ui/MinistryCard.tsx
+++ src/ui/MinistryCard.tsx
@@ -1,22 +1,31 @@
+import type React from "react";
 import { IoArrowForward } from "react-icons/io5";
 
 
+interface MinistryProps {
+    title: string;
+    services: number;
+    desc: string;
+    ministry: string;
+}
 
-const MinistryCard = () => {
+
+
+const MinistryCard: React.FC<MinistryProps> = ({ title, services, desc, ministry }) => {
      return (
-        <div className="flex items-start gap-2 w-[32%] py-6 px-3 border border-gray-300">
+        <div className="flex items-start gap-2 w-full lg:w-[45%] xl:w-[32%] py-6 px-4 border border-gray-300">
               {/* Namibian Logo  */}
               <div className="h-17 w-17 bg-[url('/images/namibiaLogo.png')] bg-cover bg-center">
 
               </div>
               {/* Info Div  */}
-              <div className="px-3 w-[80%] flex flex-col gap-4">
+              <div className="px-3 w-[80%] flex flex-col gap-3">
                   <div className="w-full flex items-center justify-between">
-                     <label className="text-xl font-serif font-bold text-black/80">Home Affairs</label>
-                     <div className="px-3 py-1 bg-gray-300 flex justify-center items-center rounded-md text-black/80">3 services</div>
+                     <label className="text-xl font-serif font-bold text-black/80">{title}</label>
+                     <div className="px-3 py-1 bg-gray-300 flex justify-center items-center rounded-md text-black/80">{services} services</div>
                   </div>
-                  <label>Responsible for civil registration, immigration, national documents, and internal security.</label>
-                  <label className="text-[11px] text-gray-500">Ministry of Home Affairs, Immigration, Safety and Security</label>
+                  <label>{desc}</label>
+                  <label className="text-[11px] text-gray-500">{ministry}</label>
                   {/* Button  */}
                   <div className="w-full flex justify-end mt-2">
                       <button className="py-2 rounded-md flex gap-3 items-center justify-center cursor-pointer text-black px-3 hover:bg-blue-700 hover:text-white">View Services <IoArrowForward /></button>
