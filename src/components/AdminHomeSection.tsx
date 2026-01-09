4a6c01bc5b954e2077552a892723212c90fa530e
Tjitouua <veriimapoha@gmail.com>
2026-01-09 10:36:26 +0200

    Finished with the Search Part in the admin dashboard

--- src/components/AdminHomeSection.tsx
+++ src/components/AdminHomeSection.tsx
@@ -1,10 +1,39 @@
 import type React from "react";
 import StatisticsCard from "../ui/StatisticsCard";
+import { useState } from "react";
+import { Dropdown } from "primereact/dropdown";
 
 
 
 
 const AdminHomeSection: React.FC = () => {
+
+
+
+    
+    const [selectedStatus, setSelectedStatus] = useState(null);
+    const status = [
+        { name: 'All Status', code: 'All Status' },
+        { name: 'Pending', code: 'Pending' },
+        { name: 'In Progress', code: 'In Progress' },
+        { name: 'Completed', code: 'Completed' },
+    ]
+
+
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const categories = [
+        { name: 'All Services', code: 'All Services' },
+        { name: 'Birth Certificate Application', code: 'Birth Certificate Application' },
+        { name: 'National ID Replacement', code: 'National ID Replacement' },
+        { name: 'Passport Application', code: 'Passport Application' },
+        { name: 'Social Welfare Grant', code: 'Social Welfare Grant' },
+        { name: 'Business Registration', code: 'Business Registration' },
+        { name: "Driver's License Renewal", code: "Driver's License Renewal"},
+        { name: 'Marriage Cerficate', code: 'Marriage Cerficate' },
+        { name: 'Land Title Registration', code: 'Land Title Registration' },
+    ];
+
+    
 
 
     const StatisticsCardList = [
@@ -21,12 +50,12 @@ const AdminHomeSection: React.FC = () => {
         {
             number: 1,
             category: "In Progress",
-            bgColor: "bg-red-100"
+            bgColor: "bg-green-100"
         }, 
         {
             number: 5,
             category: "Completed",
-            bgColor: "bg-orange-100"
+            bgColor: "bg-red-100"
         }, 
     ];
 
@@ -44,6 +73,14 @@ const AdminHomeSection: React.FC = () => {
                  />
                 ))}
              </div>
+             {/* Search part */}
+            <div className="w-full flex py-2 gap-4 ">
+                <input className="w-4/5 rounded-md px-5 border border-gray-300" placeholder="Search services ...." type="text" />
+                <Dropdown value={selectedStatus} onChange={(e) => setSelectedStatus(e.value)} options={status} optionLabel="name" 
+                placeholder="All Status" className="w-1/5 md:w-14rem" />
+                <Dropdown value={selectedCategory} onChange={(e) => setSelectedCategory(e.value)} options={categories} optionLabel="name" 
+                placeholder="All Services" className="w-1/5 md:w-14rem" />
+            </div>
         </div>
       );
 }
--- src/ui/StatisticsCard.tsx
+++ src/ui/StatisticsCard.tsx
@@ -11,7 +11,7 @@ interface StatisticsCardProps {
 
 const StatisticsCard: React.FC<StatisticsCardProps> = ({ number, category, bgColor = "bg-blue-100" }) => {
       return (
-        <div className={`py-4 bg-blue-100 rounded-xl flex flex-col items-center justify-center w-1/4 ${bgColor}`}>
+        <div className={`py-5 bg-blue-100 rounded-xl flex flex-col items-center justify-center w-1/4 ${bgColor}`}>
              <label className="text-xl font-serif font-bold">{number}</label>
              <label>{category}</label>
         </div>
